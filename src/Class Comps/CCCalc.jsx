43680baee78f3d42676b859bb5e7cc4018cf41fc
barkan33 <mishakolomyza@gmail.com>
2024-03-21 13:58:22 +0200

    Update CCCalc.jsx

--- src/Class Comps/CCCalc.jsx
+++ src/Class Comps/CCCalc.jsx
@@ -36,12 +36,18 @@ export default class CCalc extends Component {
                 </div>
 
                 <div className="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" onClick={this.SetOperators} value={'+'} className="btn btn-primary">+</button>
-                    <button type="button" onClick={this.SetOperators} value={'-'} className="btn btn-primary">-</button>
-                    <button type="button" onClick={this.SetOperators} value={'*'} className="btn btn-primary">*</button>
+                    <button type="button" onClick={this.SetOperators} value={'+'} className="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                    </svg></button>
+                    <button type="button" onClick={this.SetOperators} value={'-'} className="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
+                        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+                    </svg></button>
+                    <button type="button" onClick={this.SetOperators} value={'*'} className="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                    </svg></button>
                     <button type="button" onClick={this.SetOperators} value={'/'} className="btn btn-primary">/</button>
                 </div>
-                <h1 style={{margin: 10}}>{this.state.answer}</h1>
+                <h1 style={{ margin: 10 }}>{this.state.answer}</h1>
             </div>
 
         )
